8aa40a59164f0c476c22431801d736d6fb30216c
Sakthi <isakthopro@Sakthis-MacBook-Pro.local>
2021-01-16 08:22:52 +0530

    2.1.4 changes

--- ugs-pendant/src/main/webapp/e2e/src/app.e2e-spec.ts
+++ ugs-pendant/src/main/webapp/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to aags-pendant!');
+    expect(page.getTitleText()).toEqual('Welcome to ugs-pendant!');
   });
 
   afterEach(async () => {
